69b20983de354b2eba67b068f7d0e32398e3ccc1
Giga Amiridze <flameyungoxx@gmail.com>
2022-12-05 18:59:38 +0400

    Added 'Hello World' exercise

---
+++ src/exercises/00-Hello-World/HelloWorld.jsx
@@ -0,0 +1,15 @@
+/*
+  Exercise:
+    - Change the contents of the render function and save the file
+    - See the updates automatically in your browser without refreshing!
+*/
+
+import React from 'react';
+
+function HelloWorld() {
+  return (
+    <div>Hello World!</div>
+  );
+}
+
+export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+// Components
+import HelloWorld from './exercises/00-Hello-World/HelloWorld';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <HelloWorld />
   </React.StrictMode>
 );
 
